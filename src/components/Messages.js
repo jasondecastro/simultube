99260dec12f8ae0bda5d9f59e3c567962c31b418
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-03 11:47:23 -0400

    messages

---
+++ src/components/Messages.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Messages extends Component {
+  render() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-body" id="messages">
+          <p><strong>jason:</strong> hey</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Messages
