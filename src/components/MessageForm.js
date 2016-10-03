11ffb27dc7907390839caba7350d711d38c390c0
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-03 11:47:16 -0400

    message form component

---
+++ src/components/MessageForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MessageForm extends Component {
+  render() {
+    return (
+      <form>
+        <textarea className="form-control" />
+        <input type="hidden" />
+      </form>
+    )
+  }
+}
+
+export default MessageForm
