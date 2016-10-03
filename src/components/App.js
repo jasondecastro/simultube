a55ca8c83c5bd423d143577adcd040c6e4c415ef
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-03 11:46:33 -0400

    started seperating everything into their own component and refactoring the code in the App's render to reflect that

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
 import '../static/bootstrap.min.css'
 
-const buttonStyle = {
-  display: 'none'
-}
+import Chatroom from './Chatroom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-           <div className="container">
-              <div className="row col-md-8 col-md-offset-2">
-                <h1>rails</h1>
-
-                <div className="panel panel-default">
-                    <div className="panel-body" id="messages">
-                      <p><strong>jason:</strong> hey</p>
-                    </div>
-                </div>
-
-
-                  <form>
-                    <textarea className="form-control" />
-                    <input value="1" type="hidden" name="message[chatroom_id]" id="message_chatroom_id" />
-                  </form>
-                </div>
-            </div>
+        <Chatroom />
       </div>
     )
   }
