de5c190a9d21dd6a3270585a00adfe4811819a17
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-04 10:07:57 -0400

    started foundation of the actual game

---
+++ src/components/Stage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+const stageStyle = {
+  width: '900px',
+  height: '610px',
+  backgroundColor: 'black',
+  float: 'left'
+}
+
+class Stage extends Component {
+  render() {
+    return (
+      <div style={stageStyle}>
+      
+      </div>
+    )
+  }
+}
+
+export default Stage
