5e45bdecf7c7fed64cd90a19f86ed8246e2bba81
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-04 10:07:25 -0400

    styled the hallway and added doors

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router'
 
 const rooms = {
   margin: '0 auto',
-  paddingLeft: '100px'
+  paddingLeft: '15px',
+  paddingTop: '120px'
+}
+
+const doorStyle = {
+  width: '300px',
+  height: '300px',
+  marginLeft: '60px'
 }
 
 class Rooms extends Component {
@@ -37,21 +44,40 @@ class Rooms extends Component {
 
   render() {
     return (
-      <div style={rooms}>
-        <h2>
-          <Link to="/rooms/the_garden">The Garden</Link>
-        </h2>
-        <p>{this.state.topics[0]}</p>
 
-        <h2>
-          <Link to="/rooms/the_pool">The Pool</Link>
-        </h2>
-        <p>{this.state.topics[1]}</p>
-        <h2>
-          <Link to="/rooms/the_parlor">The Parlor</Link>
-        </h2>
-        <p>{this.state.topics[2]}</p>
+      <div className="row" style={rooms}>
+
+        <div className="col-md-4">
+          <center>
+            <h2>
+              <Link to="/rooms/the_garden">The Garden</Link>
+            </h2>
+            <p>{this.state.topics[0]}</p>
+          </center>
+          <img src="http://www.crdoors.net/wp-content/uploads/2015/08/door.png" style={doorStyle}/>
+        </div>
+
+        <div className="col-md-4">
+          <center>
+            <h2>
+              <Link to="/rooms/the_pool">The Pool</Link>
+            </h2>
+            <p>{this.state.topics[1]}</p>
+          </center>
+          <img src="http://www.crdoors.net/wp-content/uploads/2015/08/door.png" style={doorStyle}/>
+        </div>
+
+        <div className="col-md-4">
+          <center>
+            <h2>
+              <Link to="/rooms/the_parlor">The Parlor</Link>
+            </h2>
+            <p>{this.state.topics[2]}</p>
+          </center>
+          <img src="http://www.crdoors.net/wp-content/uploads/2015/08/door.png" style={doorStyle}/>
+        </div>
       </div>
+
     )
   }
 }
