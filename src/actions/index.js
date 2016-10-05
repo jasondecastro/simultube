9a58aad38f6cce0b32b23853a8682c3bde91b329
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-05 15:30:46 -0400

    actions for redux

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+export function fetchTopics() {
+  const url = 'http://localhost:8000/api/v1/topics'
+
+  const topics = fetch(url)
+    .then(response => {
+      return response.json()
+    })
+    .then(responseBody => {
+      const topics = responseBody.data.map(data => {
+        return data.attributes.topic
+      })
+
+      return topics
+    })
+
+  return {
+    type: 'FETCH_TOPICS',
+    payload: topics
+  }
+}
