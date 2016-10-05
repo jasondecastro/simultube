030d0eb0fc8d53ea9a9c6b74f944c2a83bbdda51
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-05 15:30:11 -0400

    implementing redux

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App'
 import Chatroom from './components/Chatroom'
 
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxPromise from 'redux-promise'
+import rootReducer from './reducers'
+import { fetchTopics } from './actions'
+
+const store = createStore(rootReducer, applyMiddleware(ReduxPromise))
+store.dispatch(fetchTopics())
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-    <Route path="/hallway" component={App} />
-    <Route path="/rooms/:id" component={Chatroom} />
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+      <Route path="/hallway" component={App} />
+      <Route path="/rooms/:id" component={Chatroom} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
