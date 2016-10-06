d293504e6084e37c1a45563e5768fff29787e1a1
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-06 16:21:03 -0400

    implemented redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,74 @@ export function fetchTopics() {
       return topics
     })
 
+  // topics = ["topic1", "topic2"]
+  
   return {
     type: 'FETCH_TOPICS',
     payload: topics
+  }
+}
+
+export function fetchMessages() {
+  const url = 'http://localhost:8000/api/v1/messages'
+
+  const messages = fetch(url,
+  {
+    method: 'GET',
+    headers: {
+      'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`
+    }
+  })
+  .then(response => {
+    return response.json()
+  }).then(responseBody => {
+    return responseBody.data
+  })
+
+  return {
+    type: 'FETCH_MESSAGES',
+    payload: messages
+  }
+}
+
+export function fetchUsers() {
+  const url = 'http://localhost:8000/api/v1/users'
+
+  const users = fetch(url,
+  {
+    headers: {
+      'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`
+    }
+  })
+  .then(response => {
+    return response.json()
+  }).then(responseBody => {
+    return responseBody.data
+  })
+
+  return {
+    type: 'FETCH_USERS',
+    payload: users
+  }
+}
+
+export function newMessage(newMessageObject) {
+  return {
+    type: 'NEW_MESSAGE',
+    payload: newMessageObject
+  }
+}
+
+export function newUser(newUserObject) {
+  return {
+    type: 'NEW_USER',
+    payload: newUserObject
+  }
+}
+
+export function changeUser(changedUserObject) {
+  return {
+    type: 'CHANGE_USER',
+    payload: changedUserObject
   }
 }
